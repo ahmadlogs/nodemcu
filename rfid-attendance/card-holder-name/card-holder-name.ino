dea24efccabc2083ab011736a05c68c4772d9f66
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2022-02-20 19:57:49 +0500

    Update card-holder-name.ino

--- rfid-attendance/card-holder-name/card-holder-name.ino
+++ rfid-attendance/card-holder-name/card-holder-name.ino
@@ -11,7 +11,7 @@ MFRC522::MIFARE_Key key;
 int blockNum = 2;  
 /* This is the actual data which is 
 going to be written into the card */
-byte blockData [16] = {"David_Prince"};
+byte blockData [16] = {"Ahmad_Logs"};
 //-----------------------------------------
 /* Create array to read data from Block */
 /* Length of buffer should be 2 Bytes 
