8df303db20db4c2dfd9be04628d36020ed0b1a4e
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2022-07-20 12:22:10 +0500

    Update student-data.ino

--- rfid-attendance3/student-data.ino
+++ rfid-attendance3/student-data.ino
@@ -1,16 +1,22 @@
 /* ------------------------------------------------------------------------
  * Created by: Tauseef Ahmad
- * Created on: 4 April, 2022
+ * Created on: 17 July, 2022
  *  
  * Tutorial: https://youtu.be/Bgs_3F5rL5Q
  * ------------------------------------------------------------------------
  * Download Resources
  * ------------------------------------------------------------------------
  * Preferences--> Aditional boards Manager URLs : 
- * For ESP8266 and NodeMCU - Version 3.0.2
+ * For ESP8266 and NodeMCU - Board Version 2.6.3
  * http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ * ------------------------------------------------------------------------
+ * HTTPS Redirect Library:
+ * https://github.com/jbuszkie/HTTPSRedirect
+ * Example Arduino/ESP8266 code to upload data to Google Sheets
+ * Follow setup instructions found here:
+ * https://github.com/StorageB/Google-Sheets-Logging
  * ------------------------------------------------------------------------*/
-
+ 
 
 #include <SPI.h>
 #include <MFRC522.h>
